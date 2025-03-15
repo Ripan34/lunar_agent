650b223851abf8d712c0f78278131dbee482fe67
Ripandeep Singh <ripandeep.singh@sjsu.edu>
2025-03-15 14:48:43 -0700

    update batch size

--- lunar.py
+++ lunar.py
@@ -12,13 +12,13 @@ state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
 
 batch_size = 32
-num_episodes = 1000
+num_episodes = 500
 target_update_frequency = 10
 min_epsilon = 0.01
 epsilon_decay = 0.995
 epsilon = 1.0
 
-agent = LunarAgent(env=env, action_size=action_dim, state_size=state_dim, epsilon=epsilon, batch_size=10000, gama=0.99, 
+agent = LunarAgent(env=env, action_size=action_dim, state_size=state_dim, epsilon=epsilon, batch_size=batch_size, gama=0.99, 
                    min_epsilon=min_epsilon, epsilon_decay=epsilon_decay)
 
 for episode in range(num_episodes):
